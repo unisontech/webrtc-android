f5932ba3fdbf54ad536aa001ca88f47452d6891c
Roman Skvirsky <romansk@users.noreply.github.com>
2014-09-05 16:56:12 +0400

    Update build-webrtc.sh
    fixed path location (after environment)

--- build-webrtc.sh
+++ build-webrtc.sh
@@ -29,8 +29,8 @@ set_environment_for_x86() {
 build() {
     echo "-- building webrtc/$1"
     pushd trunk || fail
-    ./setup_links.py --force || fail
     set_environment_for_$1 || fail
+    ./setup_links.py --force || fail
     gclient sync --force || fail
     gclient runhooks --force || fail
     ninja -C out_$1/Debug libjingle_peerconnection_so libjingle_peerconnection.jar || fail
