4e86015d9d5f7fcda9b18124c835b37444505a6a
agrusha <agrusha@unison.com>
2015-05-20 16:09:35 +0300

    automated aar arm/x86 deployment to repo

--- build-webrtc.sh
+++ build-webrtc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+revision=`cat revision`
+
 fail() {
     echo "*** webrtc build failed"
     exit 1
@@ -7,7 +9,7 @@ fail() {
 
 set_environment() {
     export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64'
-    export GYP_DEFINES="build_with_libjingle=1 build_with_chromium=0 libjingle_java=1 libjingle_objc=0 OS=android"
+    export GYP_DEFINES="build_with_libjingle=1 build_with_chromium=0 libjingle_objc=0 OS=android"
     export GYP_GENERATORS="ninja"
     export GYP_CROSSCOMPILE=1
     export MACH=`uname -m`
@@ -16,77 +18,104 @@ set_environment() {
 
 set_environment_for_arm() {
     set_environment
-    export GYP_DEFINES="$GYP_DEFINES java_home=$JAVA_HOME OS=android"
     export GYP_GENERATOR_FLAGS="$GYP_GENERATOR_FLAGS output_dir=out_arm"
-    export STRIP="$ANDROID_NDK/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-$MACH/arm-linux-androideabi/bin/strip"
-    export ABI="armeabi-v7a"
+    export STRIP="$ANDROID_NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-$MACH/arm-linux-androideabi/bin/strip"
 }
 
 set_environment_for_x86() {
    set_environment
-   export GYP_DEFINES="$GYP_DEFINES java_home=$JAVA_HOME OS=android target_arch=ia32"
+   export GYP_DEFINES="$GYP_DEFINES target_arch=ia32"
    export GYP_GENERATOR_FLAGS="$GYP_GENERATOR_FLAGS output_dir=out_x86"
-   export STRIP="$ANDROID_NDK/toolchains/x86-4.6/prebuilt/linux-$MACH/bin/i686-linux-android-strip"
-   export ABI="x86"
+   export STRIP="$ANDROID_NDK/toolchains/x86-4.8/prebuilt/linux-$MACH/bin/i686-linux-android-strip"
+}
+
+prerequisites() {
+    git pull || fail
+    if [ -d "depot_tools" ]; then
+        echo "depot exists"
+    else 
+        git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git || fail
+    fi
+    export PATH=`pwd`/depot_tools:"$PATH"
+    if [ -d "src" ]; then
+        echo "src exists. Fetch updates"
+        pushd src || fail        
+        git fetch || fail
+        popd
+    else 
+        echo "src not found. Fetch all sources"
+        fetch webrtc_android || fail
+    fi    
+    pushd src || fail
+    git checkout $1
+    . build/android/envsetup.sh
+    python webrtc/build/gyp_webrtc
+    popd
 }
 
 build() {
     echo "-- building webrtc/$1"
-	rm ./.gclient_entries
-    src/setup_links.py --force || fail
+    set_environment_for_$1
     pushd src || fail
-    set_environment_for_$1 || fail
-    gclient sync --force || fail
-    gclient runhooks --force || fail
-    ninja -C out_$1/Debug AppRTCDemo libjingle_peerconnection.jar || fail
-    ninja -C out_$1/Release AppRTCDemo libjingle_peerconnection.jar || fail
-    $STRIP -s out_$1/Release/AppRTCDemo/libs/$ABI/libjingle_peerconnection_so.so || fail
+    python webrtc/build/gyp_webrtc
+    ninja -C out_$1/Debug libjingle_peerconnection_so libjingle_peerconnection.jar || fail
+    ninja -C out_$1/Release libjingle_peerconnection_so libjingle_peerconnection.jar || fail
+    $STRIP -s out_$1/Release/lib/libjingle_peerconnection_so.so || fail
     pushd out_$1/Release || fail
     popd
     popd
-    echo "-- webrtc/$1 has been sucessfully built"
+    echo "-- webrtc/$1 has been successfully built"
 }
 
-prerequisites() {
-    export PATH=`pwd`/depot_tools:"$PATH"
-    which gclient >/dev/null
-    if [ $? -ne 0 ]; 
-    then
-	   git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git || fail
-	   export PATH=`pwd`/depot_tools:"$PATH"
+init_mvn_repo() {
+    if [ -d "repo" ]; then
+        echo "repo exists"
+    else 
+        mkdir repo || fail
     fi
-    set_environment
-    gclient sync --with_branch_heads || fail
-    pushd src 
-    git checkout -b release_$RELEASE_VERSION refs/remotes/branch-heads/$RELEASE_VERSION
-    source ./build/android/envsetup.sh
-    popd
 }
 
-pushToGit() {
-    REVISION=`grep -Po '(?<==)[^\"]+' release-version`
-    pushd repo    
-    git add --all
-    git commit -m "webrtc revision: $REVISION"
-    git push origin repo || fail
-    popd
-    git add --all
-    git commit -m "webrtc revision: $REVISION"
-    git push origin master || fail
+cleanDiff() {
+    if [ -f "patches/$1.diff" ]; then
+        echo "previous diff for revision $1 found"
+        rm -f patches/$1.diff
+    else 
+        echo "no diff for revision $1 found."
+    fi 
 }
 
-prerequisites
+checkForPatch() {
+    if [ -f "patches/$1.diff" ]; then
+        echo "diff for revision $1 found"
+    else 
+        echo "no diff for revision $1 found. Have you applied patches?"
+        fail
+    fi 
+}
+
+build_aar() {
+    pushd aar-project || fail
+    echo "arr build for rev: $1"
+    ./gradlew -Prevision=$1 build
+    popd	
+}
+
+
+echo "Building revision: $revision"
+
+prerequisites $revision
 
 build arm
 build x86
 
-pushd repo
-git pull origin repo
-popd
+init_mvn_repo
 
-./deploy_webrtc.scala
+cleanDiff $revision
 
-pushToGit
+make -B repo
 
+checkForPatch $revision
 
+build_aar $revision
 
+make -B aar
